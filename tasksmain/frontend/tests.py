7f58566c62775e1b6e88c966d172e7830fde8125
Egor <129303990+egobzh@users.noreply.github.com>
2023-11-12 23:43:32 +0300

    Update tests.py

--- tasksmain/frontend/tests.py
+++ tasksmain/frontend/tests.py
@@ -15,14 +15,14 @@ class UserTestCase(TestCase):
     def test_is_ok_task_add(self):
         headers = {'task': 'тест запси!'}
         response = self.c.post('/task_add', {'task': 'тест запси!'})
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
     def test_is_ok_task_complete(self):
         add = self.c.post('/task_add', {'task': 'тест запси!'})
         response = self.c.post('/tasks_commit/1')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
     def test_is_ok_task_uncomplete(self):
         add = self.c.post('/task_add', {'task': 'тест запси!'})
         complete = self.c.post('/tasks_commit/1')
         response = self.c.post('/tasks_commit/1')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
 
